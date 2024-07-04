00b4b5e48c57db7016008f3c1f2fca385dc11096
Noah Dolev <noah@faim.online>
2024-07-04 15:01:46 +0200

    updated init py

--- lmao/__init__.py
+++ lmao/__init__.py
@@ -1,5 +1,7 @@
 from .ingest_repo import *
 from .ingest_slack import *
 from .ingest_office import *
+from .ingest_jira import *
+from .ingest_confluence import *
 
 # __all__ = ['RepoIngestor', 'repo_main', 'SlackIngestor', 'slack_main', 'OfficeIngestor', 'office_main']s
